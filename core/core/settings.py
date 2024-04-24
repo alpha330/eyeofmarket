32e245d6192b286485efefa55ddb8f41c5e037bf
alimahmoodi <alimahmoodi22@gmail.com>
2024-04-24 21:44:49 +0330

    update for general django structure
    add debugger
    in url
    settings
    requirements

--- core/core/settings.py
+++ core/core/settings.py
@@ -146,3 +146,15 @@ EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD",default="")
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+SHOW_DEBUGGER_TOOLBAR=config("SHOW_DEBUGGER_TOOLBAR",cast=bool,default=False)
+# Debugger toolbar config
+if SHOW_DEBUGGER_TOOLBAR:
+    INSTALLED_APPS += [
+        "debug_toolbar",
+    ]
+    MIDDLEWARE += [
+        "debug_toolbar.middleware.DebugToolbarMiddleware",
+    ]
+    import socket  # only if you haven't already imported this
+    hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+    INTERNAL_IPS = [ip[: ip.rfind(".")] + ".1" for ip in ips] + ["127.0.0.1", "10.0.2.2"]
