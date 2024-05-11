11995c6093500375b42f69161467f0fab50128f1
alimahmoodi <alimahmoodi22@gmail.com>
2024-05-11 22:27:25 +0330

    updates

--- core/core/settings.py
+++ core/core/settings.py
@@ -162,7 +162,7 @@ if SHOW_DEBUGGER_TOOLBAR:
     import socket  # only if you haven't already imported this
     hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
     INTERNAL_IPS = [
-        ip[: ip.rfind(".")] + ".1" for ip in ips] + ["127.0.0.1", "10.0.2.2"]
+        ip[: ip.rfind(".")] + ".1" for ip in ips] + ["127.0.0.1", "10.0.2.2","*"]
 
 # OVERDRIVE USER MODEL BY ACCOUNTS APP
 AUTH_USER_MODEL = "accounts.User"
