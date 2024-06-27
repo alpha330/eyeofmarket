9d8fbfdf4a35e1d09a3d2dcde133319e2f5bda5f
alimahmoodi <alimahmoodi22@gmail.com>
2024-06-27 15:56:44 +0330

    Update forms.py

--- core/website/forms.py
+++ core/website/forms.py
@@ -1,16 +1,20 @@
 from django import forms
 from website.models import TicketingFormModel,NewsLetterModel
-from django_recaptcha.widgets import ReCaptchaV2Checkbox
+from django_recaptcha.widgets import ReCaptchaV2Checkbox,ReCaptchaV3
 from django_recaptcha.fields import ReCaptchaField
+from django.conf import settings
 
 class TicketForm(forms.ModelForm):
     
-    captcha = ReCaptchaField(
-        public_key="6LcLzgEqAAAAAEBMBYp9VmWnP4UCqVlbnGxMmeBP",
-        private_key="6LcLzgEqAAAAAFBGfbQzPBeM21rM8sPJvKQ26-cx",
-        widget=ReCaptchaV2Checkbox,
-        required=True,
-        )
+    captcha = ReCaptchaField(public_key=settings.GOOGLE_RECAPTCHA_SITE_KEY,
+                             private_key=settings.GOOGLE_RECAPTCHA_SECRET_KEY,
+                                widget=ReCaptchaV3(
+                                    attrs={
+                                        'required_score':0.85,
+                                    }
+                                ),
+                            required=True,
+                            )
     
     class Meta:
         model = TicketingFormModel
